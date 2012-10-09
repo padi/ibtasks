f04414aaa0b88c270281d06e90c1b7b36bb2c095
Marc Rendl Ignacio <boracay+marc@aelogica.com>
2012-10-09 14:13:06 +0800

    Added NavigationController with TableViewController as rootViewController

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,12 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @window = UIWindow.alloc.initWithFrame UIScreen.mainScreen.bounds
+
+    storyboard = UIStoryboard.storyboardWithName "Storyboard", bundle: nil
+
+    @window.rootViewController = storyboard.instantiateInitialViewController
+
+    @window.makeKeyAndVisible
     true
   end
 end
---
+++ app/controllers/task_list_view_controller.rb
@@ -0,0 +1,3 @@
+class TaskListViewController < UITableViewController
+
+end
