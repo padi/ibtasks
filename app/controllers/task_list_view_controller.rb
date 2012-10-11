05257db1b712ed5b46887bd06a7b9d807a6aacba
Marc Rendl Ignacio <boracay+marc@aelogica.com>
2012-10-11 15:52:38 +0800

    Added connection required for TaskListViewController
    - complied minimally with UITableViewDelegate and UITableViewDatasource protocol

--- app/controllers/task_list_view_controller.rb
+++ app/controllers/task_list_view_controller.rb
@@ -1,3 +1,19 @@
 class TaskListViewController < UITableViewController
+  extend IB
 
+  def tableView tableView, cellForRowAtIndexPath: indexPath
+    @reuseIdentifier ||= "CELL_IDENTIFIER"
+
+    cell = tableView.dequeueReusableCellWithIdentifier(@reuseIdentifier) || begin
+      UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuseIdentifier)
+    end
+
+    cell.setText NSString.stringWithFormat "I am cell #{indexPath.row}"
+
+    cell
+  end
+
+  def tableView tableView, numberOfRowsInSection: section
+    10
+  end
 end
